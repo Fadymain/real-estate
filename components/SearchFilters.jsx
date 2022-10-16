3788e992506238abf70511083e26b2105e06b119
Adetola <adetolafadairo@gmail.com>
2022-10-15 17:28:10 -0700

    maping the filters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -3,12 +3,32 @@ import { Flex,Select, Box, Text, Input, Spinner, Icon, Button } from "@chakra-ui
 import { useRouter } from "next/router";
 import {MdCancel} from 'react-icons/md';
 import Image from "next/image";
+import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
+    const [filters, setFilters] = useState(filterData);
+
+    const searchProperties = (filterValues) => {
+
+    }
+
     return (
-        <div>
-            searchFilters
-        </div>
+        <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+            {filters.map((filter) => {
+                <Box key={filter.queryName}>
+                    <Select 
+                    placeholder={filter.placeholder}
+                    w='fit-content'
+                    p='2'
+                    onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })}
+                    >
+
+                    </Select>
+                </Box>
+            })}
+        </Flex>
     )
 }
+
+
 export default SearchFilters;
