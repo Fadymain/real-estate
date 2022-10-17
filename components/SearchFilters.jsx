2d0dc24c1406934333a2f96c8273223179d83fbb
Adetola <adetolafadairo@gmail.com>
2022-10-16 21:47:24 -0700

    mapping through items to be displayed in select

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Flex,Select, Box, Text, Input, Spinner, Icon, Button } from "@chakra-ui/react";
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import {MdCancel} from 'react-icons/md';
 import Image from "next/image";
@@ -14,18 +14,22 @@ const SearchFilters = () => {
 
     return (
         <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
-            {filters.map((filter) => {
+            {filters.map((filter) => (
                 <Box key={filter.queryName}>
                     <Select 
-                    placeholder={filter.placeholder}
-                    w='fit-content'
-                    p='2'
-                    onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })}
+                        placeholder={filter.placeholder}
+                        w='fit-content'
+                        p='2'
+                        onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })}
                     >
-
+                        {filter?.items?.map((item) => (
+                            <option value={item.value} key={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
                     </Select>
                 </Box>
-            })}
+            ))}
         </Flex>
     )
 }
