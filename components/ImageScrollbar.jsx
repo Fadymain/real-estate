dc4a5fa61f8679133b4298a1824348f5fca4f032
Adetola <adetolafadairo@gmail.com>
2022-10-16 23:55:00 -0700

    correcting typos

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import Image from "next/image";
 import { Box, Icon, Flex } from "@chakra-ui/react";
-import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu/dist/types";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 const LeftArrow = () => {
@@ -27,14 +27,15 @@ const RightArrow = () => {
 const ImageScrollbar = ({ data }) => (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
         {data.map((item) => (
-            <Box key={item.id} width='910px' itemID={item.id} overflow='hidden' p='1'>
+            <Box key={item.id} width='910px' itemId={item.id} overflow='hidden' p='1'>
                 <Image 
                     alt='property'
                     placeholder='blur' 
                     blurDataURL={item.url} 
-                    src={item.url} width={1000} 
+                    src={item.url} 
+                    width={1000} 
                     height={500} 
-                    sizes='(max-width:500px) 100px, (max-width:1023px) 400px, 1000px'
+                    sizes='(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px'
                     />
             </Box>
         ))}
