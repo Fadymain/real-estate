93484c13c6e9ce44596a5e6ce23ff788204a5f97
Adetola <adetolafadairo@gmail.com>
2022-10-27 12:05:49 -0700

    adding property details to display on page

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -6,9 +6,31 @@ import millify from "millify";
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
 import ImageScrollbar from "../../components/ImageScrollbar";
 
-const PropertyDetails = ({ propertyDetails: {price, rentFrequency, rooms, title, baths, area, agency, isVerfied, description, type, purpose, furnishingStatus, amenities, photos } }) => (
+const PropertyDetails = ({ propertyDetails: {price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => (
     <Box maxWidth='1000px' margin='auto' p='4'>
         {photos && <ImageScrollbar data={photos} />}
+        <Box w='full' p='6'>
+                <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
+                    <Flex alignItems={"center"}>
+                        <Box paddingRight='3' color="green.400">{isVerified && <GoVerified />}</Box>
+                        <Text fontWeight='bold' fontSize='lg'>AED {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
+                    </Flex>
+                    <Box>
+                        <Avatar size='sm' src={agency?.logo?.url}/>
+                    </Box>
+                </Flex>
+                <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
+                    {rooms} <FaBed/> | {baths} <FaBath/> | {millify(area)} sqft <BsGridFill/>
+                </Flex>
+                <Box margin='2'>
+                    <Text fontSize='lg' marginBottom='2' fontWeight='bold'>
+                        {title}
+                    </Text>
+                    <Text lineHeight='2' color='gray.600'>
+                        {description}
+                    </Text>
+                </Box>
+        </Box>
     </Box>
 );
 
